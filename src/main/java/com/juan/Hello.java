ae259fcf55d0fd3d3bce5821425d8ae5d683195e
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2020-07-01 16:13:45 +0800

    Java and Kotlin variable data type

--- src/main/java/com/juan/Hello.java
+++ src/main/java/com/juan/Hello.java
@@ -5,5 +5,20 @@ public class Hello {
         //System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19 ;
+        Integer age2 = 19; //基本資料型態的包裝型態
+        char c ='我'; //只能用單引號
+        Character c2 = 'A';
+        byte b = 120 ;
+        float weight = 66.5f; //未加 f 會被使用 double 型態
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Yuan"; //字串為 “”
+        
+
+
+
+
     }
 }
--- src/main/kotlin/com/juan/Hello.kt
+++ src/main/kotlin/com/juan/Hello.kt
@@ -3,6 +3,11 @@ package com.juan
 fun main() {
     //print("Hello Kotlin")
     Human().hello()
+    var age = 19 // var age : Int = 19 宣告資料類型可以省略
+    age = 20
+    var weight = 66.5f
+    var name : String //未初始給值必須先宣告資料類型
+    name = "Yaun"
 }
 
 class Human{
