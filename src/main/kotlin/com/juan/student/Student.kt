7ac55906cc467db89e2b9d9c8d057478055c2e59
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2020-07-09 15:32:11 +0800

    kotlin 的when(){} 方法

--- src/main/kotlin/com/juan/student/Student.kt
+++ src/main/kotlin/com/juan/student/Student.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun main(args: Array<String>) {
     //userInput()
-    val stu = Student_k("Yuan", 77, 99)
+    val stu = Student_k("Yuan", 10, 99)
     stu.print()
     //stu.nameCheck()
     //println("Hight score: "+stu.highest())
@@ -15,14 +15,35 @@ fun main(args: Array<String>) {
 
 class Student_k(var name: String? ,var english: Int ,var math: Int) {
     fun print(){
-        println(name+ "\t" + english + "\t" + math
+        println("$name\t$english\t$math\t${getAverage()}\t${passOrFaiked()}\t${grading()}")
+        /*print(name+ "\t" + english + "\t" + math
                 +"\t" +getAverage()+ "\t" +
-                if (getAverage() >= 60) "PASS" else "FAILED")
+                passOrFaiked() )//if (getAverage() >= 60) "PASS" else "FAILED")
+        println("\t"+grading())*/
     }
+    fun grading() = when(getAverage()) {  //kotlin 沒有swith 被 when 取代
+        in 90..100 -> 'A'  //90~100的值
+        in 80..89 -> 'B'
+        in 70..79 -> 'C'
+        in 60..69 -> 'D'
+        else -> 'F'
+    }
+    /*fun grading() :Char {
+        var grading :Char ='F'
+        grading = when(getAverage()) {  //kotlin 沒有swith 被 when 取代
+            in 90..100 -> 'A'
+            in 80..89 -> 'B'
+            in 70..79 -> 'C'
+            in 60..69 -> 'D'
+            else -> 'F'
+        }
+        return grading
+    }*/
 
     fun getAverage() : Int {
         return (english+math)/2
     }
+    fun passOrFaiked() = if (getAverage() >= 60) "PASS" else "FAILED"
 
     fun highest() : Int { //Kotlin  沒有三元運算
         //var max = if (math > english) math else english  //kotlin 可以把if寫在變數等號右邊
