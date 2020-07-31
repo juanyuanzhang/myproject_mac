3549c79f79d79b8a1ced680dd3504ca26bfa3286
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2020-07-31 16:10:06 +0800

    JAVA8時間類別D學習

--- src/main/java/com/juan/parking/Tester.java
+++ src/main/java/com/juan/parking/Tester.java
@@ -2,11 +2,35 @@ package com.juan.parking;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
     public static void main(String[] args) {
+        //java8
+        java8();
+        //java();
+    }
+
+    private static void java8() {
+        Instant instant = Instant.now(); // 帶有時區的物件
+        System.out.println(instant);
+        //lacal 本地時間物件
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        //格式化
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");//設定格式
+        System.out.println(formatter.format(now)); //  把本地時間格式化
+        System.out.println(now.plus(Duration.ofHours(3))); //   時間加3小時
+        LocalDateTime other = LocalDateTime.of(2020,07,31,16,07);//指定時間
+        System.out.println(other);
+    }
+
+    private static void java() {
         Date date = new Date();
         System.out.println(date);
         System.out.println(date.getTime());
